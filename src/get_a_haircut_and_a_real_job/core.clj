ba250af52fbae80ece4bef4ff3267d56b81bcacb
Colin Campbell-McPherson <colin@logaan.net>
2011-10-27 23:33:55 +1100

    failing at reading

--- src/get_a_haircut_and_a_real_job/core.clj
+++ src/get_a_haircut_and_a_real_job/core.clj
@@ -1,4 +1,5 @@
-(ns get-a-haircut-and-a-real-job.core)
+(ns get-a-haircut-and-a-real-job.core
+  (:use [clojure.contrib.shell]))
 
 (defn home []
   (print "[H"))
@@ -8,4 +9,23 @@
 
 (defn position [x y]
   (printf "[%s;%sH" y x))
+
+(defn foreground-blue []
+  (printf "[34m"))
+
+(defn background-yellow []
+  (printf "[43m"))
+
+(defn reset-all-attributes []
+  (printf "[0m"))
+
+(defn really-get-char [input-char]
+  (if (= input-char -1)
+    (recur (.read *in*))
+    (char input-char)))
+
+(defn get-char []
+  (sh "sh" "-c" "stty raw </dev/tty")
+  (really-get-char (.read *in*))
+  (sh "sh" "-c" "stty cooked </dev/tty"))
 
--- test/get_a_haircut_and_a_real_job/test/core.clj
+++ test/get_a_haircut_and_a_real_job/test/core.clj
@@ -2,9 +2,19 @@
   (:use [get-a-haircut-and-a-real-job.core])
   (:use [clojure.test]))
 
-(deftest position-the-cursor
+(deftest centered-hello-world
   (home)
   (clear)
+
   (position 35 12)
+  (foreground-blue)
+  (background-yellow)
   (print "Hello world")
+  (reset-all-attributes)
+
+  (position 2 4)
+  (print *in*)
+  ;(print (get-char))
+
   (position 0 21))
+
